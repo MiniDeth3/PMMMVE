3a50466f7310ebfa115e28fc03d24898920fb4aa
MiniDeth3 <142844055+MiniDeth3@users.noreply.github.com>
2025-05-24 00:56:09 -0500

    fixing and general functionality

--- src/data/plugins/PMMMVEModPlugin.java
+++ src/data/plugins/PMMMVEModPlugin.java
@@ -8,16 +8,12 @@ import com.fs.starfarer.api.combat.ShipHullSpecAPI;
 import org.apache.log4j.Logger;
 import org.dark.shaders.util.ShaderLib;
 import org.dark.shaders.util.TextureData;
-import scripts.PMMCrossmodScript;
-import scripts.PMMLunaSettings;
-import scripts.PMMSettingsScript;
 
 public class PMMMVEModPlugin extends BaseModPlugin {
 
     private static final Logger log = Global.getLogger(PMMMVEModPlugin.class);
     private static boolean hasGraphicsLib = false;
 
-    @Override
     public void onApplicationLoad() {
         // Handle shaders if the graphics library is enabled
         hasGraphicsLib = Global.getSettings().getModManager().isModEnabled("shaderLib");
@@ -27,73 +23,11 @@ public class PMMMVEModPlugin extends BaseModPlugin {
 
             if (ShaderLib.areShadersAllowed() && ShaderLib.areBuffersAllowed()) {
                 TextureData.readTextureDataCSV("data/config/pmm_texture_data.csv");
-                log.info("PMM shaders active");
+                log.info("PMMMVE shaders active");
             }
         }
 
         // Log the plugin initialization
         log.info("Welcome to PMMM! I'm MiniDeth3, and I'm in your logs now...");
-    }
-
-    @Override
-    public void onGameLoad(boolean wasEnabledBefore) {
-        addPirateMods();
-        initializeCrossmods();
-        updateLunaSettings();
-        // Handle ship swapping
-        handleShipSwapping();
-    }
-
-    private void addPirateMods() {
-        for (ShipHullSpecAPI hullSpec : Global.getSettings().getAllShipHullSpecs()) {
-            if (hullSpec.getManufacturer().contains("Pirate") && !hullSpec.isBuiltInMod("pmm_compmods")) {
-                hullSpec.addBuiltInMod("pmm_compmods");
-                log.info("Added Pirate Modifications to " + hullSpec.getHullNameWithDashClass());
-            }
-        }
-    }
-
-    private void initializeCrossmods() {
-        if (Global.getSettings().getModManager().isModEnabled("TouchOfVanilla_vri")) {
-            PMMCrossmodScript.initVRICrossmod();
-            log.info("So you're a teal enjoyer!");
-        }
-
-        if (Global.getSettings().getModManager().isModEnabled("aerialcombatsuit")) {
-            PMMCrossmodScript.initIndiesCrossmod();
-            log.info("Cool backwards ships!");
-        }
-
-        if (Global.getSettings().getModManager().isModEnabled("Scrapyard")) {
-            PMMCrossmodScript.initScrapyardCrossmod();
-            log.info("\"Where spacers see scrap metal, a salvager sees a fleet\"");
-        }
-
-        if (Global.getSettings().getModManager().isModEnabled("swp")) {
-            PMMCrossmodScript.initSWPCrossmod();
-            log.info("SWP is just the best pack out there");
-        }
-    }
-
-    private void updateLunaSettings() {
-        boolean omegaToggle = PMMLunaSettings.OmegaToggle();
-        boolean masterRecover = PMMLunaSettings.MasterRecover();
-        boolean pirateGlowToggle = PMMLunaSettings.PirateGlowToggle();
-        boolean afflictorChangeToggle = PMMLunaSettings.VanillaChangeToggle_Afflictor();
-
-        PMMSettingsScript.initOmega();
-        log.info(omegaToggle ? "Enabled PMM omega" : "Disabled PMM omega");
-
-        PMMSettingsScript.initMasterRec();
-        if (masterRecover) log.info("PMM Master can be recovered");
-
-        PMMSettingsScript.initVTC_afflictor();
-        if (!afflictorChangeToggle) log.info("PMM afflictor changes disabled");
-    }
-
-    private void handleShipSwapping() {
-        // Get the toggle value for modded ships from the settings
-        boolean vtcAfflictor = PMMSettingsScript.initVTC_afflictor(); // Update based on your settings script
-        boolean vtcBuffalo = PMMSettingsScript.initVTC_buffalo(); // Update based on your settings script
     }
 }
